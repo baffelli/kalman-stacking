156b0823b11978ff9a8313cae825106b6a984a88
Simone Baffelli <baffelli@ifu.baug.ethz.ch>
2016-11-09 17:56:27 +0000

    Added r function to compute variogram with or without model
    git-svn-id: https://svn.id.ethz.ch/eoethz/trunk/bafsimon/trunk/Bisgletscher_pol@2704 7821f091-807e-4826-bf48-bc026ed4e502

--- scripts/variogram.R
+++ scripts/variogram.R
@@ -4,37 +4,51 @@ library(akima)
 unw<-read.table('/home/baffelli/PhD/trunk/Bisgletscher_pol/ipt/20150803_060519_stack_20_AAAl.aps.csv', sep=',', head=T)
 #Read heights 
 hgt<-read.table('/home/baffelli/PhD/trunk/Bisgletscher_pol/ipt/20150803_060519_stack_20_AAAl.hgt.csv', sep=',', head=T)
+#Glacier
+glacier<-read.table('/home/baffelli/PhD/trunk/Bisgletscher_pol/ipt/20150803_060519_stack_20_AAAl.glacier.csv', sep=',', head=T)
+
+
+pt_hgt = data.frame(x=hgt$x,y=hgt$y, z=hgt$record_0)
+glacier_hgt = data.frame(x=glacier$x,y=glacier$y, z=glacier$record_0)
+ifgram = data.frame(x=unw$x,y=unw$y, phase=unw$record_5)
+
+
 
 
 #Color ramp for plotting
 cols <-  colorRampPalette(c("#000099", "#00FEFF", "#45FE4F","#FCFF00", "#FF9400", "#FF3100"))(256)
 #Model as a function of elevation
-model_eq <- unw$record_0 ~ hgt$x + hgt$y + I(hgt$x^2) + I(hgt$y^2) + hgt$x*hgt$y + hgt$record_0 + I(hgt$record_0^2) + I(hgt$y^2*hgt$x^2)
+model_eq <- ifgram$phase ~ pt_hgt$x + pt_hgt$y + I(pt_hgt$x^2) + I(pt_hgt$y^2) + pt_hgt$x*pt_hgt$y + I(pt_hgt$z^2) 
 model <- lm(model_eq)
-predicted_hgt = predict(model)
+predicted_hgt = predict(model, se.fit=TRUE)
+predicted_glacier = predict(model,glacier_hgt, se.fit=TRUE)
 
 #diagnostic plot
-plot(hgt$x, hgt$y, col=cols[cut(unw$record_0, breaks = seq(-5,5,l=256))])
-points(hgt$x, hgt$y, col=cols[cut(predicted_hgt, breaks = seq(-5,5,l=256))], pch=12, cex=(unw$record_0 - predicted_hgt)**2/5)
+#plot(hgt$x, hgt$y, col=cols[cut(unw$record_0, breaks = seq(-5,5,l=256))])
+plot(ifgram$x, ifgram$y, col=cols[cut(ifgram$phase- predicted_hgt$fit, breaks = seq(-5,5,l=256))], pch=12, cex=(predicted_hgt$se.fit)*20)
+plot(glacier_hgt$x, glacier_hgt$y, col=cols[cut(predicted_glacier$fit, breaks = seq(-5,5,l=256))], pch=12, cex=(predicted_glacier$se.fit)*20)
 
 
+#Diff ifgram
+res<-data.frame(x=ifgram$x,y=ifgram$y, phase=ifgram$phase - predicted_hgt$fit)
 
 #Create dataframe
-unw.geo <- as.geodata(unw, coords.col = 1:2, data.col=16)
-res.geo <- as.geodata(unw - predicted_hgt, coords.col = 1:2, data.col=16)
+unw.geo <- as.geodata(ifgram, coords.col = 1:2)
+res.geo <- as.geodata(res, coords.col = 1:2)
 #Compute variogram
-unw.var <- variog(unw.geo, breaks=seq(30, 3000, l=50))
-res.var <- variog(res.geo, breaks=seq(30, 3000, l=50))
+lags = seq(30,5000, l=50)
+unw.var <- variog(unw.geo, breaks=lags)
+res.var <- variog(res.geo, breaks=lags)
 
 
 
 #Fit model
-unw.var.fit <- variofit(unw.var, ini.cov.pars = c(0.5,1000),cov.model ="exponential",fix.nugget=FALSE, nugget=0.5)
+unw.var.fit <- variofit(unw.var, ini.cov.pars = c(0.5,1000),cov.model ="spherical",fix.nugget=FALSE, nugget=0.5)
 #Fit model
-res.var.fit <- variofit(res.var, ini.cov.pars = c(0.5,1000),cov.model ="exponential",fix.nugget=FALSE, nugget=0.5)
+res.var.fit <- variofit(res.var, ini.cov.pars = c(0.5,2000),cov.model ="spherical",fix.nugget=FALSE, nugget=0.5)
 
 #Plot models
-plot(unw.var,ylim=c(0,10))
+plot(unw.var,ylim=c(0,2))
 lines(res.var, col='red')
 lines.variomodel(unw.var.fit)
 lines.variomodel(res.var.fit,col='red')
