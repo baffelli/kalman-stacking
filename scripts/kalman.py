0b43ca94037c1a0db15ff38eb4eb4eed7139d1d9
Simone Baffelli <baffelli@ifu.baug.ethz.ch>
2016-12-13 17:38:28 +0000

    Working 'skeleton' of a kalman filter uisng snakemake
    git-svn-id: https://svn.id.ethz.ch/eoethz/trunk/bafsimon/trunk/Bisgletscher_pol@2848 7821f091-807e-4826-bf48-bc026ed4e502

--- scripts/kalman.py
+++ scripts/kalman.py
@@ -24,24 +24,40 @@ def kalman(input, output, threads, config, params, wildcards):
     #load kalman state
     x = np.genfromtxt(input.x)
     P = np.genfromtxt(input.P)
+    print(P)
     #load inputs
     dt = []
     master_times = []
     slave_times = []
     z = []
     #Create itab
-    itab = np.genfromtxt(input.itab)
+    itab = intfun.Itab.fromfile(input.itab)
     #Read in the input
     for unw_par, unw in zip(input.diff_pars, input.unw):
         unw = ifgrams.Interferogram(unw_par, unw)
-        z.append(unw[ref_pos])
+        z.append(unw[ref_pos[0] + 30, ref_pos[1] + 30])
         dt.append(unw.temporal_baseline)
-        master_times.append(unw.master_mli_par.start_time)
-        slave_times.append(unw.slave_mli_par.start_time)
+        master_times.append(unw.master_par.start_time)
+        slave_times.append(unw.slave_par.start_time)
+    print(z)
     #Read mli parameters of first and last in stack
     stack_dt = master_times[0] - slave_times[-1]
+    #Matrix for the stack transition
     F = intfun.F_model(stack_dt)
-    print(F)
+    #H matrix
+    phase_factor = np.pi * 4 / gpf.lam(unw.master_par.radar_frequency)
+    H_m = np.array([1,0]) * phase_factor
+    H = intfun.H_stack(intfun.F_model, H_m , itab,  master_times)
+    #Covariance matrix
+    R = np.eye(H.shape[0]) * 1e-3 * phase_factor
+    Q = np.eye(2) * 1e-5
+    filter = ka.KalmanFilter(2, H.shape[0], H=H, F=F, R=R, x0=x, Q=Q, P=P)
+    #Prediction step
+    filter.predict()
+    #Update
+    filter.update(z)
+    x1 = np.savetxt(output.x, filter.x)
+    P1 = np.savetxt( output.P, filter.P)
 
 
 
