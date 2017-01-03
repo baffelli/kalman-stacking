60173bac40bcaa17407b92feae501cefd2f0ae57
Simone Baffelli <baffelli@ifu.baug.ethz.ch>
2017-01-03 09:02:30 +0000

    git-svn-id: https://svn.id.ethz.ch/eoethz/trunk/bafsimon/trunk/Bisgletscher_pol@2890 7821f091-807e-4826-bf48-bc026ed4e502

---
+++ scripts/prepare_kalman.py
@@ -0,0 +1,56 @@
+from pyrat.diff import kalman as ka
+import pyrat.diff.intfun as intfun
+import numpy as np
+import matplotlib.pyplot as plt
+import itertools as it
+import scipy as sp
+import numpy as np
+import pyrat.fileutils.gpri_files as gpf
+import os
+import matplotlib.colors as cols
+
+import pyrat.core.corefun as cf
+
+import pyrat.geo.geofun as gf
+import json
+
+import pyrat.diff.core as ifgrams
+
+
+import pyrat.visualization.visfun as vf
+
+def kalman(input, output, threads, config, params, wildcards):
+    #Load reference position
+    with open(input.reference_coord,'r') as infile:
+        ref_pos = gf.get_reference_coord(json.load(infile))
+    #load inputs
+    dt = []
+    master_times = []
+    slave_times = []
+    z = []
+    #Create itab
+    itab = intfun.Itab.fromfile(input.itab)
+    #Read in the input stack
+    stack = ifgrams.Stack(input.diff_pars, input.unw, input.mli_pars, input.itab)
+    #get number of states
+    nstates = config['kalman']['nstates']
+    ifgram_shape = stack[0].shape
+    #Reshape to npixels * size
+    z = np.dstack(stack.stack).reshape((np.prod(ifgram_shape),) + (len(stack.stack),))
+    stack_dt = stack[0].master_par.start_time - stack[-1].slave_par.start_time
+    #Matrix for the stack transition
+    F = intfun.F_model(stack_dt)
+    #H matrix
+    phase_factor = np.pi * 4 / gpf.lam(stack[0].master_par.radar_frequency)
+    H_m = np.array([1,0]) * phase_factor
+    H = stack.H_stack(intfun.F_model, H_m)
+    #Save the matrices
+    H.save(output.H)
+    z.save(output.z)
+    F.save(output.F)
+
+
+kalman(snakemake.input, snakemake.output, snakemake.threads, snakemake.config, snakemake.params,
+      snakemake.wildcards)
+
+
